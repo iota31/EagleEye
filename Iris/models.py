b67752712a3c9d1924480239cb589ac6005444dc
tushar311095 <tusharshukla.3110@gmail.com>
2018-04-14 22:43:44 +0530

    django basic model implemented with admin interface. Views and UI is postponed for later.

--- Iris/models.py
+++ Iris/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible
+class Master(models.Model):
+    Room = models.PositiveIntegerField(primary_key=True, )
+    Occupancy = models.IntegerField(blank=False)
+    Occupied = models.IntegerField(blank=False, default=0)
+
+    def __str__(self):
+        return self.Room

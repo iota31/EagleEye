6f11efb24284a7246a8387abfb681d0a1d0020e5
tushar311095 <tusharshukla.3110@gmail.com>
2018-04-15 23:09:27 +0530

    django basic model implemented with admin interface. Views and UI is postponed for later.

--- Iris/models.py
+++ Iris/models.py
@@ -9,9 +9,10 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Master(models.Model):
-    Room = models.PositiveIntegerField(primary_key=True, )
+    Room = models.PositiveIntegerField(primary_key=True)
+    Name = models.CharField(blank=False, unique=True, max_length=20, default="GOT")
     Occupancy = models.IntegerField(blank=False)
     Occupied = models.IntegerField(blank=False, default=0)
 
     def __str__(self):
-        return self.Room
+        return "%s : %s" %(self.Room, self.Name)
--- Iris/views.py
+++ Iris/views.py
@@ -5,8 +5,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Master
 
 
 def index(request):
     return HttpResponse("Test for EagleEye.")
 
+
+def home(request):
+    posts = Master.objects.all()
+    return render(request, 'home.html', {'posts' : posts})
